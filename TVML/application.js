50363abfda8304cc6dfc3624f82d820442a56c88
Jiapeng Chi <jiapengchi@gmail.com>
2019-05-16 00:43:31 -0400

    Add files via upload

--- TVML/application.js
+++ TVML/application.js
@@ -1650,17 +1650,17 @@ function openVideoWindow(aid, detail, imageURL, page, title, desc, isBangumi = n
 		nowPlayer = myPlayer;
 		myPlayer.playlist = videoList;
 		myPlayer.addEventListener('timeBoundaryDidCross', (listener, extraInfo) => {
-		    console.log("bound: " + listener.boundary);
-		}, {});
-	
-		myPlayer.addEventListener('timeDidChange', function(listener,extraInfo) {
-		    console.log("time: " + listener.time);
-		},{interval: 1});
-		myPlayer.addEventListener('stateDidChange', function(listener, extraInfo) {
-			console.log("state: " + listener.state);
-		},{});
-		myPlayer.addDanMu(msg="This is a test", color=0xFF0000, fontSize=25, style=0);
-		myPlayer.play()	
+            console.log("bound: " + listener.boundary);
+        }, time_array);
+        myPlayer.addEventListener('timeDidChange', function(listener,extraInfo) {
+            console.log("time: " + listener.time);
+        },{interval: 1});
+        myPlayer.addEventListener('stateDidChange', function(listener, extraInfo) {
+            console.log("state: " + listener.state);
+        },{});
+        myPlayer.addDanMu(msg="This is a test", color=0xFF0000, fontSize=25, style=0);
+        //style 0:normal 1:top 2:bottom
+        myPlayer.play()
 }
 
 var SEC1 = '94aba54af9065f71de72f5508f1cd42e';
